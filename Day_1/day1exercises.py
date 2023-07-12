5e608a5814143faff7a86721dc5bcddf79eb5011
Ivana Ogando <iogando@gradiant.org>
2023-07-12 10:22:47 +0200

    Updated structure

---
+++ Day_1/day1exercises.py
@@ -0,0 +1,61 @@
+"""
+day1exercise1.py
+
+Project: None
+
+Maintainer Ivana Ogando González (you@you.you)
+Created @ Tuesday, 11th July 2023 10:49:59 am
+
+Copyright (c) 2023 Your Company
+All Rights Reserved
+"""
+# %%%%%%%%%%%%%%% EXERCISE 1 %%%%%%%%%%%%%%%
+
+# 2. Open the python interactive shell and do the following operations. The operands are 3 and 4.
+
+print(3 + 4)  # addition(+)
+print(3 - 4)  # subtraction(-)
+print(3 * 4)  # multiplication(*)
+print(3 / 4)  # division(/)
+print(3**4)  # exponential(**)
+print(3 % 4)  # modulus(%)
+print(3 // 4)  # Floor division operator(//)
+
+# 3. Write strings on the python interactive shell. The strings are the following:
+print("\n")
+print("Ivana")  # Your name
+print("Ogando")  # Your family name
+print("Spain")  # Your country
+
+
+# 4. Check the data types of the following data:
+print("\n")
+print(type(10))  # Int
+print(type(9.8))  # Float
+print(type(3.14))  # Float
+print(type(4 - 4j))  # Complex number
+print(type(["Asabeneh", "Python", "Finland"]))  # List
+print(type("Ivana"))  # String
+print(type("Ogando"))  # String
+print(type("Spain"))  # String
+print("\n")
+print(type({"name": "Asabeneh"}))  # Dictionary
+print(type({9.8, 3.14, 2.7}))  # Set
+print(type((9.8, 3.14, 2.7)))  # Tuple
+print("\n")
+
+# %%%%%%%%%%%%%%% EXERCISE 2 %%%%%%%%%%%%%%%
+
+# Find an Euclidean distance between (2, 3)
+number1 = 2
+number2 = 3
+
+print("The euclidean distance between %d and %d is:" % (number1, number2))
+print((number2 - number1) ** 2)
+
+# Find an Euclidean distance between (10, 8)
+number1 = 10
+number2 = 8
+
+print("The euclidean distance between %d and %d is:" % (number1, number2))
+print((number2 - number1) ** 2)
